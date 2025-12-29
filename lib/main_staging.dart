ac885e56aa9ad96e3f156028f5410ca409cf33fd
Junsuk Oh <junsuk.oh@survivalcoding.com>
2025-12-29 18:14:29 +0900

    feat: Firebase 코어, Firestore, Storage, Auth 종속성을 추가하고 앱 초기화 시 Firebase를 설정하며, 관련 설정 파일과 `.gitignore`, `launch.json`을 업데이트

--- lib/main_staging.dart
+++ lib/main_staging.dart
@@ -1,10 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flavor_memo_app/core/config/flavor_config.dart';
 import 'package:flavor_memo_app/core/di/di_setup.dart';
 import 'package:flavor_memo_app/core/router/router.dart';
+import 'package:flavor_memo_app/firebase_options.dart';
 import 'package:flavor_memo_app/my_app.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   FlavorConfig.appFlavor = Flavor.staging;
   setupDI();
 
